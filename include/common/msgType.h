9c1f06d6042a6a3fa679f99e81afc4ccf5750cd6
aggarwalanubhav <aggarwalanubhav98@gmail.com>
2021-06-22 13:03:14 +0530

    added incoming sv message structures

--- include/common/msgType.h
+++ include/common/msgType.h
@@ -17,6 +17,7 @@ extern "C"{
 #include "s11_structs.h"
 #include "s1ap_structs.h"
 #include "s1ap_ie.h"
+#include "../../src/gtpV2Codec/msgClasses/gtpV2MsgDataTypes.h"
 
 #define REQ_ARGS 0x0000
 
@@ -772,6 +773,31 @@ struct db_req_Q_msg {
     uint8_t eps_bearer_ids[DED_BEARER_COUNT];
     struct pco pco;
 };
+
+struct ps_to_cs_res_Q_msg {
+    gtp_incoming_msg_data_t header;
+    int sv_mme_cp_teid;
+    SrvccCauseIeData srvcc_cause;
+    uint32_t msc_ip;
+    TeidCIeData teid_c;
+    TargetToSourceTransparentContainerIeData target_to_source_transparent_container;
+}
+
+struct ps_to_cs_comp_noti_Q_msg {
+    gtp_incoming_msg_data_t header;
+    int sv_mme_cp_teid;
+    unsigned char IMSI[BINARY_IMSI_LEN];
+    SrvccCauseIeData srvcc_cause; 
+}
+
+struct ps_to_cs_cancel_ack_Q_msg {
+    gtp_incoming_msg_data_t header;
+    int sv_mme_cp_teid;
+    uint8_t cause;
+    
+    bool svFlagsIePresent;
+    SvFlagsIeData sv_flags;
+}
 
 #define GTP_READ_MSG_BUF_SIZE sizeof(gtp_incoming_msg_data_t)
 
